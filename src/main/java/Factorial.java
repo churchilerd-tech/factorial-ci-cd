6fbc10016bbd05dedaf6db28cccdb8f19f2b34f2
winston <churchilerd@gmail.com>
2026-09-24 12:32:26 +0530

    Fix Maven jar and add Docker deployment

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,22 +1,57 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
 public class Factorial {
 
-    public static int calculate(int n) {
+    public static long calculate(int n) {
 
-        int result = 1;
-
-        for (int i = 1; i <= n; i++) {
-            result = result * i;
+        if (n < 0) {
+            throw new IllegalArgumentException("Number cannot be negative");
         }
 
-        return result;
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int number = 5;
+        int port = Integer.parseInt(
+                System.getenv().getOrDefault("PORT", "8080")
+        );
 
-        int result = calculate(number);
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress("0.0.0.0", port),
+                0
+        );
 
-        System.out.println("Factorial = " + result);
+        server.createContext("/", exchange -> {
+
+            String response =
+                    "Factorial CI/CD Application is Running!\n" +
+                    "Factorial of 5 = " + calculate(5);
+
+            exchange.sendResponseHeaders(
+                    200,
+                    response.getBytes().length
+            );
+
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.close();
+        });
+
+        server.start();
+
+        System.out.println(
+                "Server started on port " + port
+        );
     }
 }
